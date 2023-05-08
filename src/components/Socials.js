cbf5df9fc52816613855689ecb9d88fac7593447
Madan raj <125730836+Madanraj0519@users.noreply.github.com>
2023-05-08 23:39:55 +0530

    changes in public

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -53,7 +53,7 @@ const Socials = () => {
           <BsFillPersonLinesFill size={32} />
         </>
       ),
-      href: "/Rezume (1).pdf",
+      href: "/Rezume.pdf",
       // style: "rounded-br-md",
       style:"hover:text-red-400 hover:scale-125 duration-200",
       download: true,
